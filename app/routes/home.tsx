fd7ace29c43315369a7b1f22f1b7362619bc3b09
Sean Flanagan <flanspan11@gmail.com>
2025-08-02 14:23:28 -0400

    feat: add biome script and run formatter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,9 @@
-import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+export function meta({ ...props }: Route.MetaArgs) {
+  console.log(props);
+  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
 }
 
 export default function Home() {
